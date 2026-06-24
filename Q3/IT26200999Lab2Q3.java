c64b646e60dcc5c6d7c9a957340468ff4b850886
it26200999-UKDDAyagama <it26200999@my.sliit.lk>
2026-06-24 12:54:28 +0530

    Add files via upload

---
+++ Q3/IT26200999Lab2Q3.java
@@ -0,0 +1,10 @@
+public class IT26200999Lab2Q3 {
+    public static void main(String[] args) {
+        double sideA = 3;
+        double sideB = 4;
+
+        double hypotenuse = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+
+        System.out.println("Hypotenuse: " + hypotenuse);
+    }
+}
